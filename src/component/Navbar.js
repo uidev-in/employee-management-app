b042848b982a19cb834dc701d700b2a11a7b68dc
uidev-in <mail2loveseries@gmail.com>
2023-10-28 20:59:36 +0530

    added y=temp table

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,11 +4,10 @@ import SearchBar from './SearchBar'
 export default function Navbar() {
   return (
     <header>
-    <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+    <nav className="bg-black border-gray-200 px-4 lg:px-6 py-2.5">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <Link to="/" className="flex items-center">
-                <img src="https://www.gstatic.com/images/branding/product/2x/contacts_2022_48dp.png" className="mr-3 h-6 sm:h-9" alt="" />
-                <span className="self-center text-xl font-semibold whitespace-nowrap ">Contacts</span>
+                <img src="https://www.weblly.in/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="" />
             </Link>
             <div className="flex items-center  lg:order-2">
                 <Link to="/create" className="text-white bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none ">
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary-black":"#181818",
+        "primary-gray":"#212529",
+      }
+    },
   },
   plugins: [],
 }
